def61f7d32a106db466d50a9ad742163118ef3f4
coldfish <pubg69089@gmail.com>
2021-11-20 18:43:17 +0530

    added user profile controller and routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const homeController = require('../controllers/home_controller');
-
 const router = express.Router();
 console.log('router loded');
 router.get('/',homeController.home);
+router.use('/user',require('./user'));
 
 module.exports = router;
