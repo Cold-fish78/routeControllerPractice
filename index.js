a2ea33cdd8fcd78531df547ee052c07fb4a58e04
coldfish <pubg69089@gmail.com>
2021-11-28 11:34:22 +0530

    now user will be remain logged in even after the server restarts

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ const db = require('./config/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo');
 app.use(express.urlencoded());
 // setting up view engine (ejs)
 app.set('view engine', 'ejs');
 app.set('views', './views');
+// mongostore is used to store the session cookie in  the db\\
 app.use(session({
     name: 'codeial',
     // TODO change the secret before deployment to production
@@ -20,7 +22,16 @@ app.use(session({
     resave: false,
     cookie: {
         maxAge: (1000 * 60 * 100)
-    }
+    },
+    store : MongoStore.create({
+      mongoUrl:'mongodb://localhost/codeial_development1'
+
+    },
+    {
+        function(err){
+            console.log(err || 'setup ok')
+        }
+    })
 }));
 app.use(passport.initialize());
 app.use(passport.session());
